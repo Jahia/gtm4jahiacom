fdab19fc02cf79b0259936f1c76bd38f20a3164a
Hervé Duchesne <hduchesne@jahia.com>
2023-03-22 20:12:46 +0100

    review cta

--- src/main/resources/javascript/gtm4JahiaCom.js
+++ src/main/resources/javascript/gtm4JahiaCom.js
@@ -12,22 +12,39 @@ window.addEventListener('DOMContentLoaded', (event) => {
         {name:'main_cta_upgrade', value:'upgrade'},
         {name:'main_cta_webinar', value:'webinar'},
     ]
-    document.addEventListener("click", event => {
-        const target = event.target;
-        const isCtaNode = target.nodeName === 'BUTTON' || target.nodeName === 'A';
-        if(isCtaNode){
-            const cta = target.closest('.main_cta');
-            if (cta) {
-                window.dataLayer.push({
-                    event: 'click_main_cta',
-                    cta_type: window.gtm4.cta_types.find(({name}) => cta.classList.contains(name))?.value || 'default',
-                    cta_origin: window.location.href,
-                    cta_label: cta.innerText.trim()
-                });
-            }
+    const gtm4_tracked_ctas = []
+    document.querySelectorAll('a.main_cta, button.main_cta, .main_cta a, .main_cta button, span.main_cta').forEach(element => {
+        const cta = element.closest('.main_cta');
+        if(!gtm4_tracked_ctas.includes(cta)){
+            gtm4_tracked_ctas.push(cta);
+            element.addEventListener("click", event => {
+                if (cta) {
+                    window.dataLayer.push({
+                        event: 'click_main_cta',
+                        cta_type: window.gtm4.cta_types.find(({name}) => cta.classList.contains(name))?.value || 'default',
+                        cta_origin: window.location.href,
+                        cta_label: cta.innerText.trim()
+                    });
+                }
+            })
         }
-
     });
+
+    // document.addEventListener("click", event => {
+    //     const target = event.target;
+    //     const isCtaNode = target.nodeName === 'BUTTON' || target.nodeName === 'A' || target.childElementCount === 0;
+    //     if(isCtaNode){
+    //         const cta = target.closest('.main_cta');
+    //         if (cta) {
+    //             window.dataLayer.push({
+    //                 event: 'click_main_cta',
+    //                 cta_type: window.gtm4.cta_types.find(({name}) => cta.classList.contains(name))?.value || 'default',
+    //                 cta_origin: window.location.href,
+    //                 cta_label: cta.innerText.trim()
+    //             });
+    //         }
+    //     }
+    // });
 
     //Event view_main_cta
 })
