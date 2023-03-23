d648efd39bf73eb9851fa0d54a513a53832e209f
Hervé Duchesne <hduchesne@jahia.com>
2023-03-23 20:22:11 +0100

    create filter, add jahians cookie, update cta script

---
+++ src/main/java/org/jahia/se/modules/gtm4jahia/Gtm4JahiaFilter.java
@@ -0,0 +1,185 @@
+package org.jahia.se.modules.gtm4jahia;
+
+import net.htmlparser.jericho.*;
+import org.apache.commons.lang.StringUtils;
+import org.jahia.services.content.JCRContentUtils;
+import org.jahia.services.content.JCRNodeWrapper;
+import org.jahia.services.content.JCRPropertyWrapper;
+import org.jahia.services.content.JCRValueWrapper;
+import org.jahia.services.render.RenderContext;
+import org.jahia.services.render.Resource;
+import org.jahia.services.render.filter.AbstractFilter;
+import org.jahia.services.render.filter.RenderChain;
+import org.jahia.services.render.filter.RenderFilter;
+import org.jahia.services.usermanager.JahiaUser;
+import org.jetbrains.annotations.NotNull;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.RepositoryException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component(service = RenderFilter.class)
+public class Gtm4JahiaFilter extends AbstractFilter {
+    private static Logger logger = LoggerFactory.getLogger(Gtm4JahiaFilter.class);
+    private final static String GTM4JAHIA_MODULE="gtm4jahia";
+    private final static String GTM4JAHIA_USER_COOKIE_NAME = "internal_jahian_user";
+    private final static String GTM4JAHIA_USER_COOKIE_VISITOR_VALUE = "0";
+    private final static String GTM4JAHIA_USER_COOKIE_JAHIANS_VALUE = "1";
+
+    private final static String PAGE_CATEGORY_1_PROPS="pageType";
+    private final static String PAGE_CATEGORY_2_PROPS="j:nodename";
+
+    @Activate
+    public void activate() {
+        setPriority(-1);
+        setApplyOnModes("live,preview");//,preview
+        setApplyOnConfigurations("page");
+        setApplyOnTemplateTypes("html");
+        setSkipOnConfigurations("include,wrapper");//?
+    }
+
+    @Override
+    public String execute(String previousOut, RenderContext renderContext, Resource resource, RenderChain chain) throws Exception {
+        String output = super.execute(previousOut, renderContext, resource, chain);
+        boolean isInstalled = false;
+
+        JCRPropertyWrapper installedModules = renderContext.getSite().getProperty("j:installedModules");
+
+        for (JCRValueWrapper module : installedModules.getValues()) {
+            if (GTM4JAHIA_MODULE.equals(module.getString())) {
+                isInstalled = true;
+                break;
+            }
+        }
+
+        //Disable the filter in case we are in Content Editor preview.
+        boolean isCEPreview = renderContext.getRequest().getAttribute("ce_preview") != null;
+
+        if(isInstalled && !isCEPreview){
+            //cookie management
+            manageCookie(renderContext);
+
+            //update output to add scripts
+            output = enhanceOutput(output, renderContext);
+        }
+
+
+        return output;
+    }
+
+    /**
+     * This Function is just to add some logic to our filter and therefore not needed to declare a filter
+     *
+     * @param output    Original output to modify
+     * @return          Modified output
+     */
+    @NotNull
+    private String enhanceOutput(String output, RenderContext renderContext) throws Exception{
+
+        Source source = new Source(output);
+        OutputDocument outputDocument = new OutputDocument(source);
+
+        //Add webapp script to the HEAD tag
+        List<Element> elementList = source.getAllElements(HTMLElementName.HEAD);
+        if (elementList != null && !elementList.isEmpty()) {
+            final StartTag headStartTag = elementList.get(0).getStartTag();
+            outputDocument.replace(headStartTag.getEnd(), headStartTag.getEnd() + 1, getHeadScript(renderContext));
+        }
+
+        output = outputDocument.toString().trim();
+        return output;
+    }
+
+    private String getHeadScript(RenderContext renderContext) throws RepositoryException {
+        List<String> pageCategories = getPageCategories(renderContext);
+        StringBuilder headScriptBuilder =
+                new StringBuilder("\n<script type=\"application/javascript\">");
+
+        if(!pageCategories.isEmpty())
+            headScriptBuilder.append("\nwindow.gtm4 = window.gtm4 || {};gtm4.pageInfo = {event: 'info',page_category_1: '"+pageCategories.get(0)+"',page_category_2: '"+pageCategories.get(1)+"'}");
+
+        headScriptBuilder.append( "\n</script>");
+        headScriptBuilder.append( "\n<script async type=\"text/javascript\" src=\"/modules/gtm4jahia/javascript/gtm4JahiaCom.js\"></script>\n<" );
+        return headScriptBuilder.toString();
+    }
+
+    private List<String> getPageCategories(RenderContext renderContext) throws RepositoryException {
+        List<String> pageCategories = new ArrayList<String>();
+        JCRNodeWrapper mainResourceNode = renderContext.getMainResource().getNode();
+        JCRNodeWrapper pageNode;
+
+        if(mainResourceNode.isNodeType("jnt:page")) {
+            pageNode = mainResourceNode;
+        }else{
+            pageNode = JCRContentUtils.getParentOfType(mainResourceNode,"jnt:page");
+        }
+
+        if(pageNode != null) {
+            pageCategories.add(
+                pageNode.hasProperty(PAGE_CATEGORY_1_PROPS) ?
+                    pageNode.getProperty(PAGE_CATEGORY_1_PROPS).getValue().getNode().getProperty("jcr:title").getValue().toString()
+                    : "n/a"
+            );
+            pageCategories.add(pageNode.getProperty(PAGE_CATEGORY_2_PROPS).getValue().toString());
+        }
+        return pageCategories;
+    }
+
+    private void manageCookie(RenderContext renderContext){
+        HttpServletRequest httpServletRequest = renderContext.getRequest();
+        List<Cookie> cookieNextPreviewList = Arrays.stream(httpServletRequest.getCookies()) // convert list to stream
+                .filter(cookie -> cookie.getName().equals(GTM4JAHIA_USER_COOKIE_NAME))
+                .collect(Collectors.toList());
+        //add cookie
+        if (cookieNextPreviewList.isEmpty()){
+            if(isJahians(renderContext)){
+                renderContext.getResponse().addCookie(
+                        buildCookie(GTM4JAHIA_USER_COOKIE_JAHIANS_VALUE,getCookiePath(httpServletRequest))
+                );
+            }else{
+                renderContext.getResponse().addCookie(
+                        buildCookie(GTM4JAHIA_USER_COOKIE_VISITOR_VALUE,getCookiePath(httpServletRequest))
+                );
+            }
+        }else{
+            //update cookie if needed
+            Cookie cookie = cookieNextPreviewList.get(0);
+            if(cookie.getValue().equals(GTM4JAHIA_USER_COOKIE_VISITOR_VALUE) && isJahians(renderContext))
+                renderContext.getResponse().addCookie(
+                        buildCookie(GTM4JAHIA_USER_COOKIE_JAHIANS_VALUE,getCookiePath(httpServletRequest))
+                );
+        }
+    }
+
+    private Cookie buildCookie(String value, String path){
+        Cookie cookie = new Cookie(GTM4JAHIA_USER_COOKIE_NAME,value);
+//        cookie.setValue(value);
+        cookie.setPath(path);
+        cookie.setMaxAge(365*24*60*60);//1y in sec
+        return cookie;
+    }
+//    private Cookie buildCookie(Cookie cookie, String value, String path){
+//        cookie.setValue(value);
+//        cookie.setPath(path);
+//        cookie.setMaxAge(365*24*60*60);//1y in sec
+//        return cookie;
+//    }
+    private boolean isJahians(RenderContext renderContext){
+        JahiaUser user = renderContext.getUser();
+        String email = user.getProperty("j:email");
+        return (StringUtils.isNotEmpty(email) && email.contains("@jahia.com"));
+    }
+    private String getCookiePath(HttpServletRequest httpServletRequest){
+        String cookiePath = StringUtils.isNotEmpty(httpServletRequest.getContextPath()) ?
+                httpServletRequest.getContextPath() : "/";
+        return cookiePath;
+    }
+}
--- src/main/resources/javascript/gtm4JahiaCom.js
+++ src/main/resources/javascript/gtm4JahiaCom.js
@@ -1,7 +1,8 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     window.dataLayer = window.dataLayer || [];
     //Event info
-    window.dataLayer.push(window.gtm4.pageInfo);
+    if(window.gtm4?.pageInfo)
+        window.dataLayer.push(window.gtm4.pageInfo);
 
     //Event click_main_cta
     window.gtm4.cta_types = [
@@ -10,25 +11,42 @@ window.addEventListener('DOMContentLoaded', (event) => {
         {name:'main_cta_contact', value:'contact'},
         {name:'main_cta_ressource', value:'ressource'},
         {name:'main_cta_upgrade', value:'upgrade'},
-        {name:'main_cta_webinar', value:'webinar'},
+        {name:'main_cta_webinar', value:'webinar'}
     ]
     const gtm4_tracked_ctas = []
-    document.querySelectorAll('a.main_cta, button.main_cta, .main_cta a, .main_cta button, span.main_cta').forEach(element => {
-        const cta = element.closest('.main_cta');
-        if(!gtm4_tracked_ctas.includes(cta)){
+    document.querySelectorAll('a, button').forEach(element => {
+        const cta = element.closest('.main_cta') || element.querySelector('.main_cta');
+        if (cta && !gtm4_tracked_ctas.includes(cta)) {
             gtm4_tracked_ctas.push(cta);
             element.addEventListener("click", event => {
-                if (cta) {
-                    window.dataLayer.push({
-                        event: 'click_main_cta',
-                        cta_type: window.gtm4.cta_types.find(({name}) => cta.classList.contains(name))?.value || 'default',
-                        cta_origin: window.location.href,
-                        cta_label: cta.innerText.trim()
-                    });
-                }
+                // event.preventDefault();
+                window.dataLayer.push({
+                    event: 'click_main_cta',
+                    cta_type: window.gtm4.cta_types.find(({name}) => cta.classList.contains(name))?.value || 'default',
+                    cta_origin: window.location.href,
+                    cta_label: cta.innerText.trim()
+                });
+                return true;
             })
         }
     });
+
+    // document.querySelectorAll('a.main_cta, button.main_cta, .main_cta a, .main_cta button, span.main_cta').forEach(element => {
+    //     const cta = element.closest('.main_cta');
+    //     if(!gtm4_tracked_ctas.includes(cta)){
+    //         gtm4_tracked_ctas.push(cta);
+    //         element.addEventListener("click", event => {
+    //             if (cta) {
+    //                 window.dataLayer.push({
+    //                     event: 'click_main_cta',
+    //                     cta_type: window.gtm4.cta_types.find(({name}) => cta.classList.contains(name))?.value || 'default',
+    //                     cta_origin: window.location.href,
+    //                     cta_label: cta.innerText.trim()
+    //                 });
+    //             }
+    //         })
+    //     }
+    // });
 
     // document.addEventListener("click", event => {
     //     const target = event.target;
